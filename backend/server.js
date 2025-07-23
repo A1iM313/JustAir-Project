3a003bf3d6c551bb7084ea85f1738ffc86bb7221
Maha Uddin <Student@Admins-MacBook-Pro.local>
2025-07-23 13:56:30 -0400

    backend is running

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,14 @@
-import express from "express"
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
 
-const app = express()
+const app = express();
+app.use(cors());
+app.use(express.json());
 
-app.listen(5000, () => {
-    console.log("Server OK")
-})
+// Simple healthcheck endpoint
+app.get('/', (req, res) => res.send('JustAir backend running'));
+
+// Start server
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server listening on port ${PORT}`));
